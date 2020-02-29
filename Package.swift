32dfdb293be1516e7423a57ea80ebe517f6d50b7
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-28 23:27:51 -0600

    Do not require Catalina.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "sqlite-kit",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_14),
         .iOS(.v13)
     ],
     products: [
